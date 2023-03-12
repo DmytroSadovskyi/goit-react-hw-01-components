b6247b87d0094195d0c10ce3231ebcf74a68808e
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-03-12 19:36:25 +0200

    add task 1 and 2

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import {} from './Statistics.styled';
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section>
+      {title && <h2>{title}</h2>}
+      <ul>
+        {stats.map(stat => (
+          <li key={stat.id}>
+            <span>{stat.label}</span>
+            <span>{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
